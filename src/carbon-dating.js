6871b09ed3f8fb0d3fb0b32b79fe10647d2d9362
Roma Bubnov <kilkun@mail.ru>
2023-04-17 22:52:10 +0400

    feat: add new solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  if (typeof sampleActivity === "string" || sampleActivity instanceof String) { // проверка что передаваемое значение является числом 
+    if (sampleActivity > 0 && sampleActivity < 15.1) {  // проверка на актуальность числа
+      let result = Math.log(MODERN_ACTIVITY / sampleActivity) / k; // основная формула
+      return Math.ceil(result); // округление результата
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(num) {
+  let arr = [];
+  let min = 0;
+
+  while (num) {
+    arr.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let item = 0;
+    for (let j = arr.length - 1; j >= 0; j--) {
+      if (j !== i) {
+        item = item * 10 + arr[j];
+      }
+    }
+    min = Math.max(item, min);
+  }
+
+  return min;
 }
 
 module.exports = {
